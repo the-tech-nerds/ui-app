4e9d1fcf9cbfe0302eda44968d6839f679e829b1
ranasl62 <ranasl62@gmail.com>
2021-04-22 00:44:02 +0600

    resolve the previous merge issue

--- src/app/product/controllers/product.controller.ts
+++ src/app/product/controllers/product.controller.ts
@@ -4,14 +4,14 @@ import {ProductService} from '../services/product.service';
 
 @Controller()
 export class ProductController {
-    constructor(private categoryService: ProductService) {
+    constructor(private productService: ProductService) {
     }
-
     @Get('product/search')
     @Render('product/search')
     async getProductItemsBySearchParamView(
         @Param('q') query: string
     ) {
+        console.log(query);
         return {
             query: query
         };
@@ -21,6 +21,17 @@ export class ProductController {
     async getProductItemsBySearchParam(
         @Param('q') query: string
     ) {
-        return this.categoryService.getProductsBySearchParam(query);
+        console.log(query);
+        return this.productService.getProductsBySearchParam(query);
+    }
+    @Get('product/:slug')
+    @Render('product/[product]')
+    async getProductBySlug(
+        @Param('slug') slug: string
+    ) {
+        const product = await this.productService.getProductBySlug(slug);
+        return {
+            product,
+        };
     }
 }
