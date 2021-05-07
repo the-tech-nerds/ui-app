b8ae553713211689228eb0b0ae6309e841c56907
Sakibur <sakibur@sixads.net>
2021-05-08 00:46:33 +0600

    Fix product fetch issue

--- api/product.ts
+++ api/product.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 import { AxiosDefaultResponse, Product } from "types";
 
 export const fetchProductDetailsApi = (slug: string): Promise<AxiosDefaultResponse<Product>> => {
-    return axios.get(`product-details/${slug}`).then(res => res).catch(err => err);
+    return axios.get(`/product-details/${slug}`).then(res => res).catch(err => err);
 }
