3fb89bf3843efeb585962e32f213269394e766eb
Sakibur <sakibur@sixads.net>
2021-05-23 05:11:13 +0600

    Shop image add and other optimizations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,17 +20,14 @@ const initialActions = () => {
 NProgress.configure({ showSpinner: true });
 
 Router.events.on('routeChangeStart', () => {
-  console.log('onRouteChangeStart triggered');
   NProgress.start();
 });
 
 Router.events.on('routeChangeComplete', () => {
-  console.log('onRouteChangeComplete triggered');
   NProgress.done();
 });
 
 Router.events.on('routeChangeError', () => {
-  // console.log('onRouteChangeError triggered');
   NProgress.done();
 });
 
