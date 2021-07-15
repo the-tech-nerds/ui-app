83af3e5d076366e2aacf07c6dc40dc0923834974
arifulcuet11 <arifulcuet52@gmail.com>
2021-07-15 22:23:07 +0600

    code refactor

--- pages/views/address.tsx
+++ pages/views/address.tsx
@@ -4,7 +4,6 @@ import Head from 'next/head';
 import '../../components/styles/user_dashbord.module.scss'
 import { AddressBook } from '../../components/pages/address-book';
 import {fetchUserAddressApi} from "../../api/address";
-import {OfferItem} from "../../types";
 
 
 const Address = ({ address }: any) => (<div>
@@ -17,7 +16,7 @@ const Address = ({ address }: any) => (<div>
 
 
 Address.getInitialProps = async (ctx: any) => {
-    let item: OfferItem= ctx.query.address?.data || null;
+    let item = ctx.query.address?.data || null;
     if(!item){
         const res = await fetchUserAddressApi();
         item = res?.data?.address?.data
